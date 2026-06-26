033f220691937e17a904a708b33049a66bbc69c1
yatvive <215081267+yatvive@users.noreply.github.com>
2026-06-27 00:27:00 +0100

    Update Footer.jsx

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,8 +1,15 @@
 import React from 'react'; 
-
+import Footer from './Footer'; // inclure le composant Footer comme attribut comme ci-dessous
+import './EventPlanner.css'; 
 const Footer = () => 
 { return ( <>
-
+        <section>
+            <div>
+                <p>
+                     © Organisation Event Planner. Tous droits réservés.
+                </p>
+            </div>
+        </section>
  </> )}
 
-export default Footer
+export default Footer;
